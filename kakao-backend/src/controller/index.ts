da86f9d7ed5d4e6716945bea85e3e9ce2f987181
shinddong <shinkent@naver.com>
2022-03-13 23:18:16 +0900

    변경 메시지

--- kakao-backend/src/controller/index.ts
+++ kakao-backend/src/controller/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import schoolController from "./school.controller";
-import todoController from "./todo.controller";
+
 const router = express.Router();
 router.use("/schools", schoolController); // /schools/ 이 구존데, 라우터 거치면서 /schools 지워지고 남은거 /하나.
 
