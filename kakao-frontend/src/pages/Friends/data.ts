64c3a7572fe7575b0bbacf750887f1b277eee233
shinddong <shinkent@naver.com>
2022-03-31 12:08:46 +0900

    변경 메시지

---
+++ kakao-frontend/src/pages/Friends/data.ts
@@ -0,0 +1,63 @@
+type FriendType = {
+  id: number;
+  name: string;
+  statusMessage: string;
+};
+
+export const friends: FriendType[] = [
+  {
+    id: 1,
+    name: "kim",
+    statusMessage: "ㅇ",
+  },
+  {
+    id: 2,
+    name: "lee",
+    statusMessage: "k",
+  },
+  {
+    id: 3,
+    name: "lee2",
+    statusMessage: "d",
+  },
+  {
+    id: 4,
+    name: "lee3",
+    statusMessage: "b",
+  },
+  {
+    id: 5,
+    name: "lee4",
+    statusMessage: "f",
+  },
+  {
+    id: 6,
+    name: "lee5",
+    statusMessage: "d",
+  },
+  {
+    id: 7,
+    name: "lee6",
+    statusMessage: "a",
+  },
+  {
+    id: 8,
+    name: "lee7",
+    statusMessage: "b",
+  },
+  {
+    id: 9,
+    name: "lee8",
+    statusMessage: "f",
+  },
+  {
+    id: 10,
+    name: "lee9",
+    statusMessage: "d",
+  },
+  {
+    id: 11,
+    name: "park",
+    statusMessage: "asap",
+  },
+];
